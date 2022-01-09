7e5e2dd7688208c5bef3be118b8700672a858f51
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-01-09 18:33:42 +0530

    Time: 109 ms (7.07%), Space: 17.2 MB (26.26%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -3,23 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        prev = None
-        curr = head
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        sentinel = ListNode(0)
+        sentinel.next = head
         
+        prev, curr = sentinel, head
         while curr:
             if curr.val == val:
-                if prev: #when middle element is unwanted
-                    prev.next = curr.next
-                else: #when prev is at None, that is first elememnt is unwanted
-                    head = curr.next
-                curr = curr.next
-            else: #when no element is unwanted
+                prev.next = curr.next
+            else:
                 prev = curr
-                curr = curr.next
-                
-        return head
+            curr = curr.next
+        
+        return sentinel.next
+
         
 
         
