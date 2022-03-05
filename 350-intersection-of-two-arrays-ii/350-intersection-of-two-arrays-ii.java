352cc1b94e4e488af60288b5bd350e47ec7eb951
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-05 12:23:59 -0500

    Time: 2 ms (95.77%), Space: 43.5 MB (40.76%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -39,7 +39,7 @@ class Solution{
         
       while(i < nums1.length && j < nums2.length){
           if( nums1[i] == nums2[j]){
-              nums1[k++] = nums1[i++];
+              nums2[k++] = nums1[i++];
               j++;
           }
           else if(nums1[i] < nums2[j]){
@@ -49,6 +49,6 @@ class Solution{
               j++;
           }
       }
-        return Arrays.copyOfRange(nums1, 0, k);
+        return Arrays.copyOfRange(nums2, 0, k);
     }
 }
