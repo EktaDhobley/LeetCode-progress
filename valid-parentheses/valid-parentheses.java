3553d1d36e93601ddf06712e0b91e85e0d514012
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-06 12:45:27 -0500

    Time: 5 ms, Memory: 41.7 MB - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,8 +1,8 @@
 
-
+/*
 class Solution{
     public boolean isValid(String s){
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         HashMap<Character, Character> map = new HashMap<>();
         map.put('{','}');
         map.put('[',']');
@@ -17,6 +17,45 @@ class Solution{
             }
         }
         return stack.isEmpty();
+        
+    }
+}
+*/
+
+class Solution{
+    public boolean isValid(String s){
+        
+        Stack<Character> stack = new Stack<>();
+        
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('{','}');
+        map.put('[',']');
+        map.put('(',')');
+        
+        
+        for(Character c : s.toCharArray()){
+            if(!stack.isEmpty() && map.get(stack.peek()) == c){
+                stack.pop();
+            }
+            else{
+                stack.push(c);
+            }
+        }
+        
+        
+        
+        
+        return stack.isEmpty();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
