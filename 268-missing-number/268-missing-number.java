4430826d52bac495285989dd9c330a3d0e18a610
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-06 00:32:29 -0500

    Time: 10 ms (12.44%), Space: 51.6 MB (18.68%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,41 @@
+/*class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0 ; i < len; i++){
+            set.add(nums[i]);
+           
+        }
+        int k = -1;
+        for(int i = 0 ; i <= len ; i++){
+            if(!set.contains(i)){
+                k = i;
+            }
+        }
+         return k;
+    }
+} */
+
+class Solution{
+    public int missingNumber(int[] nums){
+        Arrays.sort(nums);
+        int len = nums.length;
+       if(nums[len-1] != nums.length){
+           return nums.length;
+       }
+        else if(nums[0] != 0){
+            return 0;
+        }
+        
+        for(int i = 1 ; i < nums.length; i++){
+            int k = nums[i-1] + 1;
+            if(nums[i] != k){
+                return k;
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+}
