ea4cbe9de9527cc4d03aac20d6abc787dd21c665
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-05 19:18:14 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (58.25%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,3 +1,4 @@
+/* Rotate groups of 4 cells
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
@@ -11,5 +12,37 @@ class Solution {
                matrix[i][j] = temp;
            }
        }
+    }
+} */
+
+//Transpose and Reverse
+
+class Solution{
+    public void rotate(int[][] matrix){
+        transpose(matrix);
+        reverse(matrix);
+    }
+    
+    public void transpose(int[][] matrix){
+        int n = matrix.length;
+        
+        for(int i =0; i < n ; i++){
+            for(int j = i+1; j < n ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    public void reverse(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0 ; i < n ; i ++){
+            for(int j = 0 ; j < n / 2 ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
     }
 }
