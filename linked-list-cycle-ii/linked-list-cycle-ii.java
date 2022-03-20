e5ee49782a3163c58a12d2df7175ec6742c5683d
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-20 19:40:27 -0400

    Time: 1 ms, Memory: 45 MB - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.java
+++ linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -9,33 +9,67 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+
+// public class Solution{
+//     public ListNode detectCycle(ListNode head){
+//         Set<ListNode> visited = new HashSet<ListNode>();
         
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            
-            if(slow == fast){
-                break;
-            }
-        }
-        
-        slow = head;
-        
-        while(fast != null && fast.next != null){
-            if(fast == slow){
-                return fast;
-            }
-            
-            fast = fast.next;
-            slow = slow.next;
-        }
-        
-        return null;
-        
-    }
+//         ListNode node = head;
+//         while(node!=null){
+//             if(visited.contains(node)){
+//                 return node;
+//             }
+//             visited.add(node);
+//             node = node.next;
+//         }
+//         return null;
+//     }
+// }
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ public class Solution {
+     public ListNode detectCycle(ListNode head) {
+         ListNode fast = head;
+         ListNode slow = head;
+        
+      while(fast != null && fast.next != null){
+             slow = slow.next;
+             fast = fast.next.next;
+            
+             if(slow == fast){
+               break;
+             }
+         }
+        
+         slow = head;
+        
+         while(fast != null && fast.next != null){
+            if(fast == slow){
+                 return fast;
+             }
+            
+             fast = fast.next;
+             slow = slow.next;
+         }
+        
+         return null;
+        
 }
+    
+ }
