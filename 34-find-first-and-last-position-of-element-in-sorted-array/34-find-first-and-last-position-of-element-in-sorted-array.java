85e1d5ee6a339a5b50e96290a60e20e45bf15303
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-29 18:57:32 -0400

    Time: 0 ms (100.00%), Space: 45.7 MB (81.27%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -63,9 +63,9 @@ class Solution {
         
         if(nums.length == 0 || nums == null) return new int[]{-1,-1};
         
-//         int arr[] = new int[2];
-//         arr[0] = -1;
-//         arr[1] = -1;
+        int arr[] = new int[2];
+        arr[0] = -1;
+        arr[1] = -1;
         
         int i = 0;
         int j = nums.length - 1;
@@ -82,8 +82,8 @@ class Solution {
             
         }
         
-        // return arr;
-          return new int[] {-1,-1};
+        return arr;
+          // return new int[] {-1,-1};
         
 }
 
