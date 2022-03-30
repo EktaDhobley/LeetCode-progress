f50abfda9be6e11c729c6d9e7d0d71cd166bd655
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-30 19:29:38 -0400

    Time: 0 ms (100.00%), Space: 42 MB (78.38%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,32 +1,68 @@
+// class Solution {
+//     public int findMin(int[] nums) {
+//         if(nums.length == 1){
+//             return nums[0];
+//         }
+        
+//         int end = nums.length-1;
+//         int start = 0;
+        
+//         if(nums[end] > nums[0]){
+//             return nums[0];
+//         }
+        
+//         while(start <= end){
+//             int mid = start + (end - start)/2 ; //straight forward easy
+//             if(nums[mid] > nums[mid + 1]){
+//                 return nums[mid+1];
+//             }
+//             else if(nums[mid] < nums[mid - 1]){ //straight forward easy
+//                 return nums[mid];
+//             }
+            
+//             if(nums[mid] > nums[0]){ //if nums[mid] > nums[0] that means we have to search on right of nums[mid] inorder to find the min element
+//                 start = mid + 1;
+//             }
+//             else{ //if nums[mid] > nums[0] that means we have to search on left of nums[mid] inorder to find the min element
+//                 end = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int findMin(int[] nums) {
-        if(nums.length == 1){
+        if(nums.length == 1)
             return nums[0];
-        }
         
-        int end = nums.length-1;
+       
         int start = 0;
+        int end = nums.length - 1;
         
-        if(nums[end] > nums[0]){
+         if(nums[end] > nums[0]){
             return nums[0];
-        }
+        } //Not rotated
+        
+    
         
         while(start <= end){
-            int mid = start + (end - start)/2 ; //straight forward easy
+            int mid = start + (end-start)/2;
+            
             if(nums[mid] > nums[mid + 1]){
                 return nums[mid+1];
             }
-            else if(nums[mid] < nums[mid - 1]){ //straight forward easy
+            else if( nums[mid] < nums[mid - 1] ){
                 return nums[mid];
             }
-            
-            if(nums[mid] > nums[0]){ //if nums[mid] > nums[0] that means we have to search on right of nums[mid] inorder to find the min element
-                start = mid + 1;
+            else if(nums[mid] > nums[0]){
+                start = mid+1;
             }
-            else{ //if nums[mid] > nums[0] that means we have to search on left of nums[mid] inorder to find the min element
-                end = mid - 1;
+            else{
+                end = mid-1;
             }
         }
         return -1;
     }
-}
+        
+    }
