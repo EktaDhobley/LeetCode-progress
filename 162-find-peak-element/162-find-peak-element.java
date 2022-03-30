92ffb650eaee74e8fa4fb3f53d3aaa160226ce1e
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-03-30 19:56:35 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (61.71%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,9 +1,27 @@
+//Approach - 1
+// public class Solution {
+//     public int findPeakElement(int[] nums) {
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             if (nums[i] > nums[i + 1])
+//                 return i;
+//         }
+//         return nums.length - 1; // returns last element index
+//     }
+// }
+
 public class Solution {
-    public int findPeakElement(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1])
-                return i;
-        }
-        return nums.length - 1;
-    }
+     public int findPeakElement(int[] nums) {
+         return search(nums, 0 , nums.length-1);
+     }
+         
+         public int search(int[] nums, int l, int r){
+             if(l == r)
+                 return l;
+             
+             int mid = (l + r)/2;
+             if(nums[mid] > nums[mid+1])
+                 return search(nums, l , mid);
+             return search(nums, mid + 1, r);
+         }
 }
+
