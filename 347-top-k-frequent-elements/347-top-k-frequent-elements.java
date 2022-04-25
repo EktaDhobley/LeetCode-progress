9983ad990fa8b95ecf90fbc6967f46e8bc301c4e
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-04-24 23:11:51 -0400

    Time: 18 ms (44.73%), Space: 50.8 MB (26.74%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,23 +1,46 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-       HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i : nums){
-            map.put(i, map.getOrDefault(i,0)+1);
-        }
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+//         for(int i : nums){
+//             map.put(i, map.getOrDefault(i,0)+1);
+//         }
         
-        Queue<Integer> heap = new PriorityQueue(
-        (a,b) -> map.get(a) - map.get(b)
-        );
+//         Queue<Integer> heap = new PriorityQueue(
+//         (a,b) -> map.get(a) - map.get(b)
+//         ); //This a custom comparator, whenever you need to order things in some custom way, you need to define a custom comparator. Comparator takes 2 inputs and returns which one is larger/smaller/equal. a-b can be +ve, 0 or -ve, which denote smaller, equal or larger.
         
-        for(int n : map.keySet()){
-            heap.add(n);
-            if( heap.size() > k) heap.poll();
-        }
+//         for(int n : map.keySet()){
+//             heap.add(n);
+//             if( heap.size() > k) heap.poll();
+//         }
         
-        int[] top = new int[k];
-        for(int i = k - 1 ; i>= 0 ; --i){
-            top[i] = heap.poll();
-        }
-        return top;
-    }
+//         int[] top = new int[k];
+//         for(int i = k - 1 ; i>= 0 ; --i){
+//             top[i] = heap.poll();
+//         }
+//         return top;
+//     }
+// }
+
+class Solution{
+     public int[] topKFrequent(int[] nums, int k) {
+         
+         HashMap<Integer, Integer> map = new HashMap<>();
+         for( int i : nums){
+             map.put(i , map.getOrDefault(i, 0) + 1);
+         }
+         
+         Queue<Integer> heap = new PriorityQueue((a,b) -> map.get(a) - map.get(b));
+         
+         for(int n : map.keySet()){
+             heap.add(n);
+             if(heap.size() > k) heap.poll();
+         }
+         int[] top = new int[k];
+         for(int i = k-1; i>= 0 ; i--){
+             top[i] = heap.poll();
+         }
+         return top;
+     }
+    
 }
