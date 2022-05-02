1ae100f8bac74bca6f3738bd52f6765226588eca
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-05-02 15:16:21 -0400

    Time: 393 ms (46.20%), Space: 0B (100.00%) - LeetHub

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -1,7 +1,15 @@
-# Write your MySQL query statement below
-SELECT distinct seat_id
-FROM Cinema
-WHERE free = 1 AND
-(seat_id - 1 in (SELECT seat_id FROM Cinema WHERE free = 1) OR
-seat_id + 1 in (SELECT seat_id FROM Cinema WHERE free = 1))
-ORDER BY seat_id
+# # Write your MySQL query statement below
+# SELECT distinct seat_id
+# FROM Cinema
+# WHERE free = 1 AND
+# (seat_id - 1 in (SELECT seat_id FROM Cinema WHERE free = 1) OR
+# seat_id + 1 in (SELECT seat_id FROM Cinema WHERE free = 1))
+# ORDER BY seat_id
+
+Select distinct seat_id
+from Cinema
+where free = 1 and
+(seat_id - 1 in (select seat_id from Cinema Where free = 1) OR
+ seat_id + 1 in (select seat_id from Cinema where free = 1))
+ order by seat_id
+ 
