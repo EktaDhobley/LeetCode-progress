ac90fe4b1009ba07cc8bd646a49252c3ec910823
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-09-20 21:00:38 -0400

    Time: 2 ms (79.79%), Space: 52.8 MB (31.99%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -12,3 +12,20 @@ class Solution {
         return -1;
     }
 }
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+//         int leftSum = 0;
+//         int sum = 0;
+        
+//         for(int i = 0; i < nums.length; i++){
+//             sum =+ nums[i];
+//         }
+//         for(int i = 0; i < nums.length; i++){
+//             if(leftSum == sum - leftSum - nums[i])
+//                 return i;
+            
+//             leftSum += nums[i];
+//         }
+//         return -1;
+//     }
+// }
