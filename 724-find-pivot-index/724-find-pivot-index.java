70665dbda5c288adc51c8196f4de6164180b0db3
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-09-20 21:04:14 -0400

    Time: 2 ms (79.79%), Space: 51.9 MB (74.47%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,31 +1,29 @@
-class Solution {
-    public int pivotIndex(int[] nums) {
-      int sum = 0;
-      int leftSum = 0;
-      for(int i = 0; i < nums.length; i++){
-          sum += nums[i];
-      }
-        for(int i = 0; i < nums.length; i++){
-          if(leftSum == sum - leftSum - nums[i]) return i;
-          leftSum += nums[i];
-      }
-        return -1;
-    }
-}
 // class Solution {
 //     public int pivotIndex(int[] nums) {
-//         int leftSum = 0;
-//         int sum = 0;
-        
+//       int sum = 0;
+//       int leftSum = 0;
+//       for(int i = 0; i < nums.length; i++){
+//           sum += nums[i];
+//       }
 //         for(int i = 0; i < nums.length; i++){
-//             sum =+ nums[i];
-//         }
-//         for(int i = 0; i < nums.length; i++){
-//             if(leftSum == sum - leftSum - nums[i])
-//                 return i;
-            
-//             leftSum += nums[i];
-//         }
+//           if(leftSum == sum - leftSum - nums[i]) return i;
+//           leftSum += nums[i];
+//       }
 //         return -1;
 //     }
 // }
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(leftSum == sum - leftSum - nums[i]) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
