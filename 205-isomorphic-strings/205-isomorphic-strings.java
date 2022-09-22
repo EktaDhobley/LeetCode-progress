6ec903157284e0d4c2249604a1c0fffa1a0f059f
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-09-22 18:35:51 -0400

    Time: 15 ms (48.66%), Space: 43.1 MB (43.37%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,22 +1,74 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+        
+//         HashMap<Character, Character> map = new HashMap<>();
+        
+//         for(int i = 0; i < s.length(); i++){
+//             if(!map.containsKey(s.charAt(i))){
+//                 map.put(s.charAt(i), t.charAt(i));
+//             }
+//             else{
+//                 if(t.charAt(i) != map.get(s.charAt(i)))
+//                 return false;
+//             }
+            
+//         }
+//         //To check if there are any duplicates in values of map
+//         List<Character> list = new ArrayList<>(map.values());
+//         Set<Character> set = new HashSet<>(list);
+//         if(list.size() != set.size()){
+//             return false;
+//         }
+//         return true;
+//     }
+// }
+
+//#2 - Better Solution
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         HashMap<Character, Character> map = new HashMap<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             if (map.containsKey(s.charAt(i))) {
+//                 if (t.charAt(i) != map.get(s.charAt(i)))
+//                     return false;
+//             } else {
+//                 if (map.containsValue(t.charAt(i)))
+//                     return false;
+//                 map.put(s.charAt(i), t.charAt(i));
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> map = new HashMap<>();
-        
-        for(int i = 0; i < s.length(); i++){
-            if(!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i), t.charAt(i));
-            }
-            else{
-                if(t.charAt(i) != map.get(s.charAt(i)))
-                return false;
-            }
-            
-        }
+     HashMap<Character, Character> map = new HashMap<>();
+     for(int i = 0; i < s.length(); i++){
+         if(!map.containsKey(s.charAt(i))){
+             map.put(s.charAt(i), t.charAt(i));
+         }
+         else{
+             if(t.charAt(i) != map.get(s.charAt(i)))
+                 return false;
+         }
+     }
         List<Character> list = new ArrayList<>(map.values());
         Set<Character> set = new HashSet<>(list);
-        if(list.size() != set.size()){
-            return false;
-        }
+        if(list.size() != set.size()) return false;
         return true;
     }
+    
 }
