f1000c8d4aa67c32998b19d7c2aa5cc332ce06a4
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-10-04 21:24:10 -0400

    Time: 87 ms (20.61%), Space: 14.1 MB (27.26%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -5,7 +5,6 @@ class Solution:
         res.append(new_number)
         #print(new_number)
         for i in range(1, len(nums)):
-            
             new_number = new_number + nums[i]
             res.append(new_number)
         return res          
