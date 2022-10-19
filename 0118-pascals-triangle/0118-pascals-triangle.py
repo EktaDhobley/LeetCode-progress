751b13da3a5cf72f00eb99244f4688b857833b0a
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-10-18 21:19:49 -0400

    Time: 57 ms (44.46%), Space: 13.9 MB (18.01%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,12 +3,12 @@ class Solution:
        #define an array 
         triangle = []
         
-        for r in range(numRows): #eg numRows = 5
-            row = [None for i in range(r + 1)] #r + 1 = 1,2..
+        for r in range(numRows): #for loop for adding rows to the final triangle
+            row = [None for i in range(r + 1)] #intiating each row with None, notice the no. of elements in the row
             row[0], row[-1] = 1, 1 #first and last = 1 always
             
-            for j in range(1, len(row) - 1):
-                row[j] = triangle[r - 1][j - 1] + triangle[r - 1][j]
-            triangle.append(row)
+            for j in range(1, len(row) - 1): #nested for loop to fill up each row
+                row[j] = triangle[r - 1][j - 1] + triangle[r - 1][j] #as seen in question animation, each element is the sum of the elements directly above it
+            triangle.append(row) #After filling up the row - append it to the main traingle
         return triangle
                 
