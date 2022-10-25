fb0d507ed35d9bbd2b2e6b3caa79bd108b9f40de
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-10-25 11:48:27 -0400

    Time: 180 ms (32.55%), Space: 15.2 MB (25.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,33 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dicti = {}
+#     #one pass hashmap/ dictionary   
+#     dicti = {}
         
+#         for i in range(len(nums)):
+#             comp = target - nums[i]
+            
+#             if comp in dicti:
+#                 return [dicti[comp], i]
+            
+#             dicti[nums[i]] = i
+            
+        
+        #two pass hashmap/dcitionary
+
+        hashmap = {}
         for i in range(len(nums)):
-            comp = target - nums[i]
-            
-            if comp in dicti:
-                return [dicti[comp], i]
-            
-            dicti[nums[i]] = i
-            
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+#         dicti = {}
         
-        
+#         for i in range(len(nums)):
+#             dicti[nums[i]] = i 
+            
+#         for i in range(len(dicti)):
+#             comp = target - nums[i]
+            
+#             if comp in dicti and dicti[comp] != i:
+#                 return [i, dicti[comp]]
