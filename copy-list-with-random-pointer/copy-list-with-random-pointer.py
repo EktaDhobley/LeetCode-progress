c1c4183b3c606426d94f7292dde38b2d4544564a
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-10-27 15:56:16 -0400

    Time: 44 ms, Memory: 15 MB - LeetHub

---
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        #Two pass solution
+        #First pass - for creating copies of each node and mapping original node to corresponding copied node
+        #Second pass - leverage hashmap/dictionary to assign random pointers in copied list 
+        #Linear time complexity - O(n), even Space Complexity is O(n)
+        
+        old_to_copy = { None : None }
+        
+        #first pass
+        cur = head
+        while cur:
+            copy = Node(cur.val) #Node constructor
+            old_to_copy[cur] = copy
+            cur = cur.next
+            
+        #second pass
+        cur = head
+        while cur:
+            #setting pointers (for random ptrs)
+            copy = old_to_copy[cur] #gives copy node of cur
+            copy.next = old_to_copy[cur.next] #will give me copy of the next node
+            copy.random = old_to_copy[cur.random] #will set pointer of copy to random using cur's random ptr  
+            cur = cur.next
+        return old_to_copy[head]      
+        
+        
+        
