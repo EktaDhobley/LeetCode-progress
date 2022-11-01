8fe3822a0df615a3bcaa8f0d87cf88a224c42118
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-01 12:53:09 -0400

    Time: 336 ms, Memory: 16.4 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -3,18 +3,35 @@ class Solution:
         nIslands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    nIslands = nIslands + self.dfs(grid, i, j)
+                nIslands = nIslands + self.dfs(grid, i, j)
         return nIslands
-    
-    
     def dfs(self, grid, i, j):
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+        if i >= len(grid) or i < 0 or j >= len(grid[i]) or j < 0 or grid[i][j] == '0':
             return 0
-        
         grid[i][j] = '0'
-        self.dfs(grid, i , j+1)
-        self.dfs(grid, i, j -1)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
         self.dfs(grid, i + 1, j)
         self.dfs(grid, i - 1, j)
         return 1
+        
+        
+        
+#         nIslands = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j] == '1':
+#                     nIslands = nIslands + self.dfs(grid, i, j)
+#         return nIslands
+    
+    
+#     def dfs(self, grid, i, j):
+#         if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+#             return 0
+        
+#         grid[i][j] = '0'
+#         self.dfs(grid, i , j+1)
+#         self.dfs(grid, i, j -1)
+#         self.dfs(grid, i + 1, j)
+#         self.dfs(grid, i - 1, j)
+#         return 1
