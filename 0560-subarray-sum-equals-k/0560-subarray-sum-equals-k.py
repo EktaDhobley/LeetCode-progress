16a3e92564b9546eb5f2c38d43f09929a88ff2e0
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-03 18:49:07 -0400

    Time: 307 ms (95.76%), Space: 16.6 MB (74.78%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        curr_sum = 0
+        prefix_sum = {0 : 1}
+        
+        
+        for n in nums:
+            curr_sum = curr_sum + n
+            diff = curr_sum - k
+            
+            res += prefix_sum.get(diff, 0) #if map doesn't have the diff, default value should be 0
+            prefix_sum[curr_sum] = 1 + prefix_sum.get(curr_sum, 0)
+        return res
+            
+        
+        
