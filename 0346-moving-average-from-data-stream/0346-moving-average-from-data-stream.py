6d5962b4f96de67dc15e187c4e930c0453dcbb6e
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-02 23:47:04 -0400

    Time: 63 ms (98.33%), Space: 17.1 MB (79.46%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,21 +1,45 @@
-class MovingAverage:
+# class MovingAverage:
     
-    #size = 3 for 1st example
-    def __init__(self, size: int):
-        self.size = size
-        self.q = []
+#     #size = 3 for 1st example
+#     def __init__(self, size: int):
+#         self.size = size
+#         self.q = []
         
 
-    def next(self, val: int) -> float:
-        size, q = self.size, self.q
-        q.append(val)
+#     def next(self, val: int) -> float:
+#         size, q = self.size, self.q
+#         q.append(val)
         
-        window_sum = sum(q[-size:])
+#         window_sum = sum(q[-size:])
         
-        return window_sum/min(len(q),size)
-        
+#         return window_sum/min(len(q),size)
+
 
 
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
+class MovingAverage:
+    
+    def __init__(self, size: int):
+        self.size = size
+        self.q = collections.deque()
+        
+        #no. of elements seen so far
+        self.window_sum = 0
+        self.count = 0
+        
+    
+    
+    def next(self, val: int) -> float:
+        
+        self.count += 1
+        
+        #cal new sum by shifting the window
+        self.q.append(val)
+        tail = self.q.popleft() if self.count > self.size else 0
+        
+        self.window_sum = self.window_sum - tail + val
+        
+        return self.window_sum/ min(self.size, self.count)
+        
