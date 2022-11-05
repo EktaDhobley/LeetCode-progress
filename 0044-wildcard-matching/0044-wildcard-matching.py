cf54d71654735f91f5b146531905038dd743d531
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-05 18:38:38 -0400

    Time: 2646 ms (5.25%), Space: 23 MB (41.54%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,42 +1,16 @@
-# class Solution:
-#     def isMatch(self, s: str, p: str) -> bool:
-#         m, n = len(s), len(p)
-        
-#         dp =[[0 for i in range(m+1) for j in range(n+1)]]
-#         dp[0][0] = 1
-        
-#         for i in range(1, n + 1):
-#             if (p[i - 1] == '*'):
-#                 dp[i][0] = True
-#             else:
-#                 break
-#         for i in range(1, n + 1):
-#             for j in range(1, m + 1):
-#                 if s[j-1] == p[i-1] or p[i-1] == '?':
-#                     dp[i][j] = dp[i-1][j-1]
-#                 elif p[i-1] == '*':
-#                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-#         return dp[n][m]
-        
-        
-        
-        
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        m,n=len(s),len(p)
-        dp=[[0 for i in range(m+1)] for j in range(n+1)]
-        dp[0][0]=1
-        for i in range(1,n + 1):
-            if(p[i - 1] == '*'):
-                dp[i][0] = True
-            else:
+    def isMatch(self, s, p):
+        dp = [[False for _ in range(len(p)+1)] for i in range(len(s)+1)]
+        dp[0][0] = True
+        for j in range(1, len(p)+1):
+            if p[j-1] != '*':
                 break
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s[j-1]==p[i-1] or p[i-1]=='?':
-                    dp[i][j]=dp[i-1][j-1]
-                elif p[i-1]=='*':
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[n][m]
+            dp[0][j] = True
                 
-        
+        for i in range(1, len(s)+1):
+            for j in range(1, len(p)+1):
+                if p[j-1] in {s[i-1], '?'}:
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
+        return dp[-1][-1]
