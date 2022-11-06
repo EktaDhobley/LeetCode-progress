e5a5c15731959243e33a384ff81277360dc135e4
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-06 11:39:32 -0500

    Time: 55 ms (69.57%), Space: 14 MB (49.21%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -4,32 +4,73 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+
+######------------- BFS --------------------------------
+# class Solution:
+#     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         
-        res = [] #store (parent,depth) tuple only of the target x and y nodes
+#         res = [] #store (parent,depth) tuple only of the target x and y nodes
                 
-        #bfs   
-        queue = deque([(root, None, 0)])
+#         #bfs   
+#         queue = deque([(root, None, 0)])
         
-        while queue:
+#         while queue:
             
-            #stop early if both targets found
-            if len(res) == 2:
-                break
+#             #minor optimisation to stop early if both targets found
+#             if len(res) == 2: #that means we found x and y both
+#                 break
             
-            node, parent, depth = queue.popleft()
+#             node, parent, depth = queue.popleft()
             
-            #if target found
-            if node.val == x or node.val == y:
-                res.append((parent, depth))
-            if node.left:
-                queue.append((node.left, node, depth + 1))
-            if node.right:
-                queue.append((node.right, node, depth + 1))
+#             #if target found
+#             if node.val == x or node.val == y:
+#                 res.append((parent, depth))
+#             if node.left:
+#                 queue.append((node.left, node, depth + 1))
+#             if node.right:
+#                 queue.append((node.right, node, depth + 1))
         
         
-        #unpack two nodes
-        node_x, node_y = res[0], res[1]
+#         #unpack two nodes
+#         #node_x, node_y = res[0], res[1] or niche wala line
+#         node_x, node_y = res
         
-        return node_x[0] != node_y[0] and node_x[1] == node_y[1]
+#         return node_x[0] != node_y[0] and node_x[1] == node_y[1]
+
+# ------------------------ DFS ----------------------
+class Solution:
+     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool: 
+            
+            res = []
+            
+            def dfs(node, parent, depth):
+                if not node:
+                    return
+                if node.val == x or node.val == y:
+                    res.append((parent, depth))
+                dfs(node.left, node, depth + 1)
+                dfs(node.right, node, depth + 1)
+            
+            dfs(root, None, 0)
+            
+            node_x, node_y = res
+            
+            return node_x[0] != node_y[0] and node_x[1] == node_y[1]
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
