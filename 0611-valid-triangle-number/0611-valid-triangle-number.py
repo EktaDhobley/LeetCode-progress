2e53bbb01864d11f702e88932505c0456b6a3877
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-07 18:17:04 -0500

    Time: 3365 ms (35.70%), Space: 13.7 MB (99.53%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -10,30 +10,30 @@ class Solution:
         
         for index_i in range( len(nums)-1, 1, -1):
             
-            third_edge = nums[index_i]
+            k = nums[index_i]
             
-            index_of_first_edge, index_of_second_edge = 0, index_i - 1
+            i, j = 0, index_i - 1
             
-            while index_of_first_edge < index_of_second_edge:
+            while i < j:
                 
-                first_edge = nums[index_of_first_edge]
-                second_edge = nums[index_of_second_edge]
+                i_ = nums[i]
+                j_ = nums[j]
                 
-                if first_edge + second_edge > third_edge:
+                if i_ + j_ > k:
                     
                     # valid triplets
-                    # first_edge    : from nums[index_of_first_edge] to nums[(index_of_second_edge-1)]
-                    # second edge   : nums[index_of_second_edge]
+                    # first_edge    : from nums[i] to nums[(j-1)]
+                    # second edge   : nums[j]
                     # third edge    : nums[index_i]
-                    valid_triplet += ( index_of_second_edge - index_of_first_edge )
+                    valid_triplet += ( j - i )
         
                     # second edge large enough
                     # make it smaller and try next run
-                    index_of_second_edge -= 1
+                    j -= 1
                 else:
                     # first edge is too small
                     # make it larger and try next run
-                    index_of_first_edge += 1
+                    i += 1
         
         
         
