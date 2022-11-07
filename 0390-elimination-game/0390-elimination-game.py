fb01db713ec0cf88543f811d456c5c6783a2f32e
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-07 11:10:18 -0500

    Time: 35 ms (99.23%), Space: 13.7 MB (96.78%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,3 +1,4 @@
+#https://www.youtube.com/watch?v=e0Og3BhJAe0
 class Solution:
     def lastRemaining(self, n: int) -> int:
         if n == 1:
@@ -5,3 +6,4 @@ class Solution:
         
         else:
             return 2*(n//2 - self.lastRemaining(n//2) + 1)
+        
