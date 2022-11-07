7c73f76d85e65339207591feae7c4552728eb466
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-07 18:12:46 -0500

    Time: 989 ms (96.53%), Space: 14 MB (28.17%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,16 +1,41 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         
+        # sort in-place
+        # keep numbers in ascending order
         nums.sort()
-        N = len(nums)
-        output = 0
         
-        for k in range(2, N):
-            i, j = 0, k - 1
-            while i < j:
-                if nums[i] + nums[j] > nums[k]:
-                    output += (j - i)
-                    j -= 1
+        # counter for valid triplet to make triangle
+        valid_triplet = 0
+        
+        for index_i in range( len(nums)-1, 1, -1):
+            
+            third_edge = nums[index_i]
+            
+            index_of_first_edge, index_of_second_edge = 0, index_i - 1
+            
+            while index_of_first_edge < index_of_second_edge:
+                
+                first_edge = nums[index_of_first_edge]
+                second_edge = nums[index_of_second_edge]
+                
+                if first_edge + second_edge > third_edge:
+                    
+                    # valid triplets
+                    # first_edge    : from nums[index_of_first_edge] to nums[(index_of_second_edge-1)]
+                    # second edge   : nums[index_of_second_edge]
+                    # third edge    : nums[index_i]
+                    valid_triplet += ( index_of_second_edge - index_of_first_edge )
+        
+                    # second edge large enough
+                    # make it smaller and try next run
+                    index_of_second_edge -= 1
                 else:
-                    i+= 1
-        return output
+                    # first edge is too small
+                    # make it larger and try next run
+                    index_of_first_edge += 1
+        
+        
+        
+        return valid_triplet
+        
