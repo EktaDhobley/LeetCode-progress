76f6954a1cabd1e2fdd9642934e12c4331d4a03b
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-06 23:56:58 -0500

    Time: 72 ms (27.56%), Space: 14.1 MB (82.50%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,29 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         output = [[]]
+        
+#         for n in nums:
+#             output = output + [curr + [n] for curr in output]
+        
+#         return output
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = [[]]
-        
-        for n in nums:
-            output = output + [curr + [n] for curr in output]
-        
-        return output
+     def subsets(self, nums: List[int]) -> List[List[int]]:
+            res = [] #list
+            
+            subset = [] #array
+            
+            def dfs(i): #i is index of nums
+                if i >= len(nums): #out of bounds we have reached our leaf nodes 
+                    res.append(subset.copy())
+                    return
+                
+                #decision to include nums[i] #left decisoion
+                subset.append(nums[i])
+                dfs(i + 1) #this call will have a different subset given to it
+                
+                #decision not to include nums[i] #right
+                subset.pop() #pop the elements just appended
+                dfs(i + 1) #this dfs call will get an empty subset passed to it
+                
+            dfs(0)
+            return res
