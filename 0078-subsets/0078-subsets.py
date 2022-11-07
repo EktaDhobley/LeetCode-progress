fd025c3ffae2717095dda16ed623712e8dd38b14
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-06 23:32:55 -0500

    Time: 31 ms (98.01%), Space: 14.1 MB (35.61%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,9 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
         output = [[]]
         
-        for num in nums:
-            output += [curr + [num] for curr in output]
+        for n in nums:
+            output += [curr + [n] for curr in output]
         
         return output
