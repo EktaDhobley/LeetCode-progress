188573df31727dbb67f75635a9190d4c4c3859ff
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-07 11:19:05 -0500

    Time: 111 ms (10.72%), Space: 13.9 MB (26.65%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -16,7 +16,7 @@ class Solution:
             if step%2==0 or n%2!=0:
                 first+=distance
 			# Multiply distance by 2
-            distance=distance<<1
+            distance=distance*2
             step+=1
 			# Divide n by 2
             n=n>>1
