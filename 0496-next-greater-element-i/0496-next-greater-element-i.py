cb15a984cd1d2557960851ce51e387d1990b3fd8
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-07 23:34:48 -0500

    Time: 105 ms (50.35%), Space: 14.1 MB (56.75%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,25 +1,41 @@
-#brute force
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = [-1]*len(nums1)
+# #brute force
+# class Solution:
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         res = [-1]*len(nums1)
         
-        for i in range(len(nums1)):
-            found = False
-            for j in range(len(nums2)):
+#         for i in range(len(nums1)):
+#             found = False
+#             for j in range(len(nums2)):
                 
-                if nums2[j] == nums1[i]:
-                    found = True
+#                 if nums2[j] == nums1[i]:
+#                     found = True
                     
-                if found and nums2[j] > nums1[i]:
-                    res[i] = nums2[j]
-                    break #break from j for loop, because we found the greater number and added to res, now we will iterate i 
+#                 if found and nums2[j] > nums1[i]:
+#                     res[i] = nums2[j]
+#                     break #break from j for loop, because we found the greater number and added to res, now we will iterate i 
     
-        return res
+#         return res
             
             
 #stack and map
-# class Solution:
-#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1] * len(nums1)
+        nums1_idx = { n:i for i, n in enumerate(nums1)} #for storing index of each number of nums1
+        stack = []
+        
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1_idx[val]
+                res[idx] = cur
+            if cur in nums1_idx: #check if cur is in nums1 then only append to stack, otherwise we dont care
+                stack.append(cur) 
+        
+        return res
+            
+            
         
         
         
