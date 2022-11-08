dd4a3d636abbb9a79cbd0a28650ed8c489d7ba68
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-07 23:00:55 -0500

    Time: 996 ms (5.00%), Space: 14.1 MB (95.70%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,6 +1,7 @@
+#brute force
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = [-1] *len(nums1)
+        res = [-1]*len(nums1)
         
         for i in range(len(nums1)):
             found = False
@@ -13,13 +14,15 @@ class Solution:
                     res[i] = nums2[j]
                     break 
             #after for loop if j reaches end of array 
-            if j == len(nums2):
-                res[i] = -1
+            # if j == len(nums2):
+            #     res[i] = -1
                     
         return res
             
             
-                
+#stack and map
+# class Solution:
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         
         
