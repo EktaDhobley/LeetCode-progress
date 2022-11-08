fc815fb1a8cd7b40a19ec72b2dc7cbb91955923f
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-08 13:21:13 -0500

    Time: 213 ms (49.04%), Space: 15.8 MB (21.82%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,15 +1,17 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        res = []
+        
+        res = [] # result
+        
         def dfs(graph, idx, curr_path, dest):
-            for v in graph[idx]:
-                if v == dest:
-                    temp = [x for x in curr_path]
-                    temp.append(v)
-                    res.append(temp)
-                else:
-                    curr_path.append(v)
-                    dfs(graph, v, curr_path, dest)
+            for v in graph[idx]: # for every vertex in our graph
+                if v == dest: #if our vertex == destination
+                    temp = [x for x in curr_path] #create a temp and store the curr_path in it
+                    temp.append(v) #append that vertex in it
+                    res.append(temp) #and append that temp to result
+                else: 
+                    curr_path.append(v) #append v to curr_path
+                    dfs(graph, v, curr_path, dest) #call dfs on that v
                     curr_path.pop() # i have seen this point, i want to go through another edge
             
         dfs(graph, 0, [0], len(graph) - 1)
