34d81c671c2e8c1c10886cd5d453c5b174546ca6
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-08 18:53:28 -0500

    Time: 262 ms (61.91%), Space: 15.2 MB (72.28%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(k + 1):
             tmpPrices = prices.copy()
             for s, d, p in flights: #source, destination, price
-                if prices[s] == float("inf"): #if we cant reach the source node
+                if prices[s] == float("inf"): #if we cant reach the source node #except the first src given in question where prices[src] = 0
                     continue
                 if prices[s] + p < tmpPrices[d]: #if we reach source node, and the price to reach node (d) after that is price to reach source node + price to reach the node (d) after source node , and if it is less than the tmpPrice[d] then we substitute it with the lower value
                     tmpPrices[d] = prices[s] + p
