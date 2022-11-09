cccd4fbd4a18cddc21ffe90df0e5c3db2dd36907
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-09 11:14:53 -0500

    Time: 98 ms (98.93%), Space: 18.6 MB (91.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,39 +1,55 @@
+
 # class Solution:
 #     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         count_map = {} #Map to map frequencies to elements
-#         freq = [[] for i in range(len(nums) + 1)] #to store frequency of each value
         
-#         for n in nums:
-#             count_map[n] = 1 + count_map.get(n, 0) #counting the numbe rof frequencies
-#         for n, c in count_map.items(): #mapping frequencies to value
-#             freq[c].append(n)
+#         res = [] #result array
+        
+#         #determines frequency of each element
+#         counts = collections.Counter(nums) #hashmap for getting frequency of each element
+#         max_freq = max(counts.values())
+        
+#         buckets = [[] for i in range(max_freq + 1)] # bucket with frequencies
+        
+#         for n, c in counts.items(): #add numbers to respective buckets
+#             buckets[c].append(n)
             
-#         res = [] #result
-        
-#         for i in range(len(freq) - 1, 0, -1): #iterating  from (len(freq) - 1) to 0
-#             for n in freq[i]:
+#         for i in range(len(buckets) - 1, 0 , -1): #iterate thriugh buckets from end (to get maximum)
+#             for n in buckets[i]:
 #                 res.append(n)
+                
 #                 if len(res) == k:
 #                     return res
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_map = {}
-        res = []
         
-        #determines frequency of each element
-        counts = collections.Counter(nums)
-        max_freq = max(counts.values())
+        freq = {}
+        h = [] #result heap
         
-        buckets = [[] for i in range(max_freq + 1)]
-        
-        for n, c in counts.items():
-            buckets[c].append(n)
-            
-        for i in range(len(buckets) - 1, 0 , -1):
-            for n in buckets[i]:
-                res.append(n)
+        for num in nums:
+            if num not in freq:
+                freq[num] = 1
+            else:
+                freq[num] += 1
                 
-                if len(res) == k:
-                    return res
-            
+        for key, count in freq.items():
+            if len(h) < k:
+                heappush(h, [count, key])
+            else:
+                heappushpop(h, [count, key])
+        return [key for count, key in h]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
             
