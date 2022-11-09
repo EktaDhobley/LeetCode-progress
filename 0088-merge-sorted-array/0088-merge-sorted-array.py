79b9976f91a6703465343c535545f4093b98f9c7
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-09 14:42:29 -0500

    Time: 73 ms (49.69%), Space: 13.8 MB (85.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,11 +7,11 @@ class Solution:
         #     nums1[i + m] = nums2[i]  
         # nums1.sort()
         
-        nums1_copy = nums1[:m] #make a copy of nums1 with only elements that are non-zero
+        nums1_copy = nums1[:m] #make a copy of nums1 with only elements are non-zero
         x,y = 0, 0
-        for p in range(n + m): #iterate throughout n+m 
-            if y >= n or (x < m and nums1_copy[x] <= nums2[y]):
-                nums1[p] = nums1_copy[x]
+        for p in range(len(nums1)): #iterate throughout n+m 
+            if y >= n or (x < m and nums1_copy[x] <= nums2[y]): #check if x and y are not out of range
+                nums1[p] = nums1_copy[x] #place the lower value in the nums1 copy
                 x += 1
             else:
                 nums1[p] = nums2[y]
