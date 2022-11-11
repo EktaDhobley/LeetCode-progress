bed1c59b52ddfe9dc3e97d58dd36da49a1e4dd3e
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-11 16:54:24 -0500

    Time: 79 ms (96.83%), Space: 17.5 MB (84.21%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,21 +1,52 @@
+# class Solution:
+#     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+#         count = 0
+#         res = 0
+        
+#         start = sorted([i[0] for i in intervals])
+#         end = sorted([i[1] for i in intervals])
+        
+#         s, e = 0, 0
+        
+#         while s < len(end):
+#             if start[s] < end[e]:
+#                 s += 1
+#                 count += 1
+#             else:
+#                 e += 1
+#                 count -= 1
+#             res = max(res, count)    
+            
+#         return res  
+#--------------------------HEAP-------------------------------------
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        count = 0
-        res = 0
         
-        start = sorted([i[0] for i in intervals])
-        end = sorted([i[1] for i in intervals])
+        #error checking
+        if not intervals:
+            return 0
         
-        s, e = 0, 0
+        #sorting acc to start time
+        intervals.sort(key = lambda interval : interval[0])
+    
+        #create heap
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
         
-        while s < len(end):
-            if start[s] < end[e]:
-                s += 1
-                count += 1
-            else:
-                e += 1
-                count -= 1
-            res = max(res, count)    
+        #iterate throguh all intervals
+        
+        for interval in intervals[1:]:
+            #if no overlap, then pop the min element in heap and push the new end time
+            if interval[0] >= heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, interval[1])
             
+        return len(heap)
         
-        return res     
+        
+        
+        
+        
+        
+        
+        
