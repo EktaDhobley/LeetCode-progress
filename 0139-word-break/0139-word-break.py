aa5893c408a7763f32127bf4f28d5cd3beda5a04
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-12 11:09:01 -0500

    Time: 47 ms (86.08%), Space: 13.9 MB (94.98%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -5,10 +5,10 @@ class Solution:
         
         for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
-                 # check if we have enough characters starting from i in string s to compare with w
+                 # check if we have enough characters starting from i in string s to compare with w, if they are, we compare
                 if (i + len(w)) <= len(s) and s[i : i+ len(w)] == w:
-                    dp[i] = dp[i + len(w)]
-                if dp[i]:
+                    dp[i] = dp[i + len(w)] #dp[0] = d[0 + len(w)] = True, since dp[4] = True (len(w) = 4)
+                if dp[i]: # we found the word, now break, and move on to next word
                     break
         return dp[0]
             
