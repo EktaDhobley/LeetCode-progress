3179a01fce97f43bcfa39736d38cb0e44c97fe7c
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-12 11:51:03 -0500

    Time: 162 ms (21.07%), Space: 13.9 MB (86.54%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -2,27 +2,30 @@
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
+# #         self.next = next
+    
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
         dummy = ListNode()
         curr = dummy
         carry = 0
-        
+        #carry shouldn't be 0, if it is we will add it to our output even if l1val and l2val are None, while loop should go on if carry != 0
         while l1 != None or l2 != None or carry != 0:
             l1val = l1.val if l1 else 0
             l2val = l2.val if l2 else 0
-            
+
             #new digit
-            value = l1val + l2val + carry
+            value = l1val + l2val + carry #15? then we get carry again
             carry = value // 10
-            rest = value % 10 #only wants ones digit
-            curr.next = ListNode(rest)
-            
+            value = value % 10
+            curr.next = ListNode(value)
+
             #update pointers
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        return dummy.next 
+
+        return dummy.next
             
         
