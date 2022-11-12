04d75cf5c276b456ee32914933a27c56e1755621
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-12 16:36:37 -0500

    Time: 50 ms (70.66%), Space: 14 MB (86.68%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -6,30 +6,75 @@
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         
-        dummy = ListNode(0, head) #created dummy node which points at head
+        dummy = ListNode(0, head)
         
-        leftPrev = dummy
-        cur = head
+        leftPrev, curr = dummy, head
         
-        # 1) reach node at position "left"
         for i in range(left - 1):
-            leftPrev = cur
-            cur = cur.next
+            leftPrev = curr
+            curr = curr.next
             
-        # 2) Now cur = "left", leftPrev = "node after left"
-        # reverse from left to right
         prev = None
         for i in range(right - left + 1):
-            tmpNext = cur.next
-            cur.next = prev
-            prev = cur
-            cur = tmpNext
+            temp = curr.next
+            curr.next = prev
+            prev, curr = curr, temp
         
-        # 3) Update pointers
-        leftPrev.next.next = cur
+        
+        leftPrev.next.next = curr
         leftPrev.next = prev
         
         return dummy.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dummy = ListNode(0, head) #created dummy node which points at head
+        
+#         leftPrev = dummy
+#         cur = head
+        
+#         # 1) reach node at position "left"
+#         for i in range(left - 1): #2-1 times iterate to reach left
+#             leftPrev = cur
+#             cur = cur.next
+            
+#         # 2) Now cur = "left", leftPrev = "node after left"
+#         # reverse from left to right
+#         prev = None
+#         for i in range(right - left + 1):
+#             tmpNext = cur.next
+#             cur.next = prev
+#             prev = cur
+#             cur = tmpNext
+        
+#         # 3) Update pointers
+#         leftPrev.next.next = cur
+#         leftPrev.next = prev
+        
+#         return dummy.next
             
         
         
