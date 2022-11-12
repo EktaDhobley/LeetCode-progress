ae5b4a17e31d30b6da4d936f4ba956fc36e9c738
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-12 10:46:07 -0500

    Time: 2019 ms (22.90%), Space: 28 MB (23.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,8 +5,10 @@ class Solution:
         curSum = 0
         
         for n in nums:
-            if curSum < 0:
+            #if we have a negative sum or negative val, we reset sum to0
+            if curSum < 0: 
                 curSum = 0
+            #otherwise add it to curSum
             curSum += n
             maxSub = max(maxSub, curSum)
         return maxSub
