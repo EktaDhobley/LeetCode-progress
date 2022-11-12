f74f87feecdc722e40f0e35a6191b1d64d8e4696
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-12 09:13:20 -0500

    Time: 2419 ms (32.64%), Space: 13.9 MB (90.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,26 +2,86 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         
         res = ""
-        res_len = 0
+        resLen = 0
         
         for i in range(len(s)):
             #odd length
             l, r = i, i
+            #considering i being the mid elem
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > res_len:
+                if (r - l + 1) > resLen:
                     res = s[l:r+1]
-                    res_len = r - l + 1
+                    resLen = r - l + 1
                 l -= 1
                 r += 1
-            #even length
+                
+            #even length 
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > res_len:
+                if (r - l + 1) > resLen:
                     res = s[l:r+1]
-                    res_len = r - l + 1
-                l -= 1
+                    resLen = r - l + 1
+                l -=1
                 r += 1
         return res
+
+                        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = ""
+#         res_len = 0
+        
+#         for i in range(len(s)):
+#             #odd length
+#             l, r = i, i
+#             while l >= 0 and r < len(s) and s[l] == s[r]:
+#                 if (r - l + 1) > res_len:
+#                     res = s[l:r+1]
+#                     res_len = r - l + 1
+#                 l -= 1
+#                 r += 1
+#             #even length
+#             l, r = i, i + 1
+#             while l >= 0 and r < len(s) and s[l] == s[r]:
+#                 if (r - l + 1) > res_len:
+#                     res = s[l:r+1]
+#                     res_len = r - l + 1
+#                 l -= 1
+#                 r += 1
+#         return res
             
 
             
