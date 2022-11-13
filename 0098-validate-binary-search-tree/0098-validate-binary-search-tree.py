0902219e1e2a3fc8f1770b33598b81b3655209db
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-13 17:27:22 -0500

    Time: 45 ms (95.36%), Space: 16.6 MB (45.40%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,36 +1,19 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
-#         def helper(node, left, right):
-#             if not node:
-#                 return True
-            
-#             if not (node.val < right and node.val > left):
-#                 return False
-            
-#             return (helper(node.left, left, node.val) and helper(node.right, node.val, right)) #boundaries for left and right using parent's limiting values
-        
-#         return helper(root, float("-inf"), float("inf"))
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
-        stack = []
-        prev = -math.inf
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        while root or stack:
-            while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
+        
+        def helper(root, left, right):
+            if not root:
+                return True
             
-            if root.val <= prev:
+            if not (root.val < right and root.val > left):
                 return False
-            else:
-                prev = root.val
-                root = root.right
-        return True
+            return helper(root.left, left, root.val) and helper(root.right, root.val, right)
+    
+        return helper(root, float("-inf"), float("inf"))
