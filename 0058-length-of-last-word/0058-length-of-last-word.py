27a2124d433ec6ccefe8324059f1f3657f654740
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-11-27 20:15:08 -0500

    Time: 33 ms (90.23%), Space: 14 MB (5.72%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,12 +1,15 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        p, length = len(s), 0
+#         p, length = len(s), 0
         
-        while p > 0:
-            p -= 1
+#         while p > 0:
+#             p -= 1
             
-            if s[p] != ' ':
-                length += 1
-            elif length > 0:
-                return length
-        return length
+#             if s[p] != ' ':
+#                 length += 1
+#             elif length > 0:
+#                 return length
+#         return length
+    
+    #pythonic solution
+        return len(s.split().pop())
