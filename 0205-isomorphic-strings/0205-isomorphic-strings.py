db0583bd9fa85217dcd21d6555dc1c52c2ea39d0
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-12-02 18:40:03 -0500

    Time: 76 ms (60.31%), Space: 14.1 MB (89.36%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,7 +1,21 @@
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         # print(len(set(s)))
+#         # print(len(set(t)))
+#         # print(set(zip(s, t)))
+#         # print(len(set(zip(s, t))))
+#         return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
+    
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # print(len(set(s)))
-        # print(len(set(t)))
-        # print(set(zip(s, t)))
-        # print(len(set(zip(s, t))))
-        return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
+        
+        hashmap = {}
+        
+        for i in range(len(s)):
+            if s[i] not in hashmap:
+                if t[i] in hashmap.values():
+                    return False
+                hashmap[s[i]] = t[i]
+            elif hashmap[s[i]] != t[i]:
+                return False
+        return True
