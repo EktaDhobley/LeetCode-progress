78db83198ec517947535293ac803263d3d570bc6
EktaDhobley <43902557+EktaDhobley@users.noreply.github.com>
2022-12-02 18:40:18 -0500

    Time: 79 ms (55.98%), Space: 14.2 MB (89.36%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -9,6 +9,8 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
+        if len(s) != len(t):
+            return False
         hashmap = {}
         
         for i in range(len(s)):
